d2e1906a558a990c2ee55484b95d8e1dd6e68309
Dimitris-Toulis <dimitritouli@gmail.com>
2021-04-14 20:07:06 +0300

    Revert "feat: webgl backend for firefox/safari"
    This reverts commit 4af0490a44d821dbb2524448c97b327bba80615a.

--- src/util/AI.ts
+++ src/util/AI.ts
@@ -1,31 +1,23 @@
-import { setBackend, ready, enableProdMode, getBackend } from "@tensorflow/tfjs-core";
+import { setBackend, ready, enableProdMode } from "@tensorflow/tfjs-core";
 import WasmDefault from "~/node_modules/@tensorflow/tfjs-backend-wasm/wasm-out/tfjs-backend-wasm.wasm?url";
 import WasmSimd from "~/node_modules/@tensorflow/tfjs-backend-wasm/wasm-out/tfjs-backend-wasm-simd.wasm?url";
 import WasmSimdThreads from "~/node_modules/@tensorflow/tfjs-backend-wasm/wasm-out/tfjs-backend-wasm-threaded-simd.wasm?url";
-import { simd } from "wasm-feature-detect";
-
+import { setWasmPaths } from "@tensorflow/tfjs-backend-wasm";
 if (import.meta.env.PROD) enableProdMode();
 function setup<T, U>(load: (config?: U) => Promise<T>, config?: U): () => Promise<T> {
 	return async () => {
-		if (await simd()) {
-			const { setWasmPaths } = await import("@tensorflow/tfjs-backend-wasm");
-			setWasmPaths({
-				"tfjs-backend-wasm.wasm": import.meta.env.DEV
-					? WasmDefault.replace("/@fs", "")
-					: WasmDefault,
-				"tfjs-backend-wasm-simd.wasm": import.meta.env.DEV
-					? WasmSimd.replace("/@fs", "")
-					: WasmSimd,
-				"tfjs-backend-wasm-threaded-simd.wasm": import.meta.env.DEV
-					? WasmSimdThreads.replace("/@fs", "")
-					: WasmSimdThreads,
-			});
-			await setBackend("wasm");
-		} else {
-			await import("@tensorflow/tfjs-backend-webgl");
-			await setBackend("webgl");
-		}
-		console.log(getBackend());
+		setWasmPaths({
+			"tfjs-backend-wasm.wasm": import.meta.env.DEV
+				? WasmDefault.replace("/@fs", "")
+				: WasmDefault,
+			"tfjs-backend-wasm-simd.wasm": import.meta.env.DEV
+				? WasmSimd.replace("/@fs", "")
+				: WasmSimd,
+			"tfjs-backend-wasm-threaded-simd.wasm": import.meta.env.DEV
+				? WasmSimdThreads.replace("/@fs", "")
+				: WasmSimdThreads,
+		});
+		await setBackend("wasm");
 		await ready();
 		return await load(config);
 	};
