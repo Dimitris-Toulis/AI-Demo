5f28c58ee82e5604a5aec7ebbad71a9dbd43a2c7
Dimitris-Toulis <dimitritouli@gmail.com>
2021-04-18 18:33:53 +0300

    feat: coco-ssd workaround in util due to https://github.com/tensorflow/tfjs/issues/4952

--- src/pages/ObjectDetector.vue
+++ src/pages/ObjectDetector.vue
@@ -1,11 +1,39 @@
 <template>
-	
+	<CommonCamera :draw="draw" :ai="ai" :startup="startup" />
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
+import CommonCamera from "../components/CommonCamera.vue";
+import CommonAI from "../util/AI";
+import { load, ObjectDetection, DetectedObject } from "../util/COCO-SSD";
 
 export default defineComponent({
-	name: 'ObjectDetector'
-})
+	name: "ObjectDetector",
+	components: {
+		CommonCamera,
+	},
+	setup: async function () {
+		const draw = async (objects: DetectedObject[], ctx: CanvasRenderingContext2D) => {
+			ctx.lineWidth = 5;
+			ctx.strokeStyle = `rgb(255,0,0)`;
+			objects.forEach((object) => {
+				ctx.lineWidth = 5;
+				const [x, y, width, height] = object.bbox;
+				ctx.strokeRect(x, y, width, height);
+				ctx.lineWidth = 0.5;
+				ctx.strokeText(`${object.class} ${(object.score * 100).toFixed(2)}%`, x, y - 10);
+			});
+		};
+		const startup = CommonAI(load);
+		const ai = (model: ObjectDetection, video: HTMLVideoElement) => {
+			return model.detect(video);
+		};
+		return {
+			startup,
+			ai,
+			draw,
+		};
+	},
+});
 </script>
