471d693b9e7a352c146b21b908c5dca5a7dde1b0
Dimitris-Toulis <dimitritouli@gmail.com>
2021-04-19 14:44:14 +0300

    fix: no error when there is no answer

--- src/pages/QNA.vue
+++ src/pages/QNA.vue
@@ -15,9 +15,10 @@
 			class="border rounded-md border-gray-500 h-[10%] text-center w-full dark:border-none"
 		/>
 		<button @click="predict()" class="bg-blue-600 h-[5%] mt-5 text-white w-full">Submit</button>
-		<ul class="list-disc p-5 dark:text-white">
+		<ul v-if="answers.length > 0" class="list-disc mt-5 p-5 dark:text-white">
 			<li v-for="answer in Array.from(answers)">{{ answer.text }}</li>
 		</ul>
+		<p v-else-if="sumbited" class="mt-5">No Answer</p>
 	</div>
 </template>
 
@@ -31,16 +32,29 @@ export default defineComponent({
 	name: "QNA",
 	setup: async () => {
 		const model = await CommonAI(load)();
+		let sumbited = ref(false);
 		let text = ref("");
 		let question = ref("");
 		let answers: Ref<Answer[]> = ref([]);
-		const predict = async () =>
-			(answers.value = await model.findAnswers(question.value, text.value));
+		const predict = async () => {
+			try {
+				answers.value = await model.findAnswers(question.value, text.value);
+			} catch (err) {
+				if (err instanceof TypeError) {
+					if (err.message.includes("Cannot read property")) answers.value = [];
+				} else {
+					throw err;
+				}
+			} finally {
+				sumbited.value = true;
+			}
+		};
 		return {
 			text,
 			question,
 			answers,
 			predict,
+			sumbited,
 		};
 	},
 });
