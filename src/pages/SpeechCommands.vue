d37864fd625c78d5bd9c57ef917f33ae56c6292c
Dimitris-Toulis <dimitritouli@gmail.com>
2021-04-20 17:45:13 +0300

    refactor: use CommonAI

--- src/pages/SpeechCommands.vue
+++ src/pages/SpeechCommands.vue
@@ -4,32 +4,14 @@
 
 <script lang="ts">
 import { defineComponent, ref } from "vue";
-import { setBackend, ready, enableProdMode } from "@tensorflow/tfjs-core";
-import WasmDefault from "~/node_modules/@tensorflow/tfjs-backend-wasm/wasm-out/tfjs-backend-wasm.wasm?url";
-import WasmSimd from "~/node_modules/@tensorflow/tfjs-backend-wasm/wasm-out/tfjs-backend-wasm-simd.wasm?url";
-import WasmSimdThreads from "~/node_modules/@tensorflow/tfjs-backend-wasm/wasm-out/tfjs-backend-wasm-threaded-simd.wasm?url";
-import { setWasmPaths } from "@tensorflow/tfjs-backend-wasm";
+import CommonAI from "../util/AI";
 import { create } from "@tensorflow-models/speech-commands";
 
 export default defineComponent({
 	name: "SpeechCommands",
 	setup: async function () {
 		let prediction = ref("Start!");
-		if (import.meta.env.PROD) enableProdMode();
-		setWasmPaths({
-			"tfjs-backend-wasm.wasm": import.meta.env.DEV
-				? WasmDefault.replace("/@fs", "")
-				: WasmDefault,
-			"tfjs-backend-wasm-simd.wasm": import.meta.env.DEV
-				? WasmSimd.replace("/@fs", "")
-				: WasmSimd,
-			"tfjs-backend-wasm-threaded-simd.wasm": import.meta.env.DEV
-				? WasmSimdThreads.replace("/@fs", "")
-				: WasmSimdThreads,
-		});
-		await setBackend("wasm");
-		await ready();
-		const model = await create("BROWSER_FFT");
+		const model = await CommonAI(async () => await create("BROWSER_FFT"))();
 		await model.ensureModelLoaded();
 		const wordLabels = model.wordLabels();
 		model.listen(async (result) => {
